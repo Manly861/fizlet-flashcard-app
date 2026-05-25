e59789cefda23cd6e61dfd9ab09e42a534c56cbb
HippoMaiDinh <lym861@hsd.k12.or.us>
2026-05-24 22:05:55 -0700

    adding basic fundamental flet code

--- src/fizlet_app/main_screen.py
+++ src/fizlet_app/main_screen.py
@@ -1,7 +1,15 @@
 import flet as ft
 
+# Set intital value, such as color, widht and height
+background_color = "#E91919"
+
 def main(page: ft.Page):
     page.title = "Hello World!"
+    page.window.height = 500
+    page.window.width = 500
+    page.window.bgcolor = background_color
     page.add(ft.Text("Hello World! I am Fizlet"))
+    page.window.resizable = False
+    page.update()
 
 ft.app(main)
