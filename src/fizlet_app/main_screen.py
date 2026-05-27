7a463116f326b21cb04d3d6b8c1c25bdc8eed863
Manly861 <lym861@hsd.k12.or.us>
2026-05-27 09:52:44 -0700

    adding font, color and size text

--- src/fizlet_app/main_screen.py
+++ src/fizlet_app/main_screen.py
@@ -1,14 +1,34 @@
 import flet as ft
 
 # Set intital value, such as color, widht and height
-background_color = "#E91919"
+background_color = "#110078"
+text_color = "#ffffff"
+start_button_color = "#e3913b"
+other_button_color = "#4f00f8"
+box_color = "#362a7f"
+text_fonts = {
+
+}
 
 def main(page: ft.Page):
-    page.title = "Hello World!"
-    page.window.height = 500
+    page.title = "Flascard App"
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    page.window.height = 600
     page.window.width = 500
-    page.window.bgcolor = background_color
-    page.add(ft.Text("Hello World! I am Fizlet"))
+    page.bgcolor = background_color
+    page.fonts = {
+        "TimesAlternative": "https://gstatic.com"
+    }
+
+    fizlet_name_display = ft.Text(
+                    "Fizlet",
+                    size = 80,
+                    color = text_color,
+                    font_family = "TimesAlternative"
+    )
+    page.add(fizlet_name_display)
     page.window.resizable = False
     page.update()
 
