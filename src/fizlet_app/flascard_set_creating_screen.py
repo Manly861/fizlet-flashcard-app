6367b82719d72e12a39eb0f6b2bdf5960ddcc990
Manly861 <lym861@hsd.k12.or.us>
2026-06-04 09:42:45 -0700

    adding term, definition input and add, done button

--- src/fizlet_app/flascard_set_creating_screen.py
+++ src/fizlet_app/flascard_set_creating_screen.py
@@ -2,13 +2,13 @@ import flet as ft
 import styles as st
 
 def main(page: ft.Page):
-    page.title = "Practicing Screen"
+    page.title = "Flascard Set Creating Screen"
     page.window.height = st.HEIGHT_SCREEN
     page.window.width = st.WIDTH_SCREEN
     page.bgcolor = st.BACKGROUND_COLOR
 
     # Create a name_input bar that ask users for name of the set
-    name_input_display = ft.Container(
+    name_input = ft.Container(
         ft.TextField(
                     label= "Name",
                     label_style= ft.TextStyle(
@@ -24,17 +24,78 @@ def main(page: ft.Page):
     )
     
     # Create a term_input bar that ask users for value of the term
+    term_input = ft.Container(
+        ft.TextField(
+                    label= "Term",
+                    label_style= ft.TextStyle(
+                        color= st.TEXT_COLOR_1,
+                        weight= ft.FontWeight.BOLD),
+                    hint_text= "Enter The Term.",
+                    hint_style= ft.TextStyle(
+                        color= st.TEXT_COLOR_2, 
+                        italic= True),                  
+                    bgcolor= st.BOX_COLOR,
+                    border_color= st.BOX_COLOR,
+        )
+    )
 
     # Create a definition_input bar that ask users for value of the definition
+    definition_input = ft.Container(
+        ft.TextField(
+                    label= "Definition",
+                    label_style= ft.TextStyle(
+                        color= st.TEXT_COLOR_1,
+                        weight= ft.FontWeight.BOLD),
+                    hint_text= "Enter The Definition.",
+                    hint_style= ft.TextStyle(
+                        color= st.TEXT_COLOR_2, 
+                        italic= True),                  
+                    bgcolor= st.BOX_COLOR,
+                    border_color= st.BOX_COLOR,
+        )
+    )
 
     # Create an add button that add a new term_input and definition_input bar
+    add_button = ft.Container(
+        content = ft.ElevatedButton(
+            content= ft.Text(
+                 "Add",
+                size = 20,
+                color= st.TEXT_COLOR_1,                
+                weight= ft.FontWeight.BOLD,    
+            ),
+            bgcolor= st.PLUS_BUTTON_COLOR,
+            style = ft.ButtonStyle(
+                shape= ft.RoundedRectangleBorder(radius=8),
+                padding=ft.Padding.symmetric(horizontal=13, vertical=6),
+            ),
+        ),
+        alignment= ft.Alignment.BOTTOM_CENTER
+    )
 
     # Create a done button that user are done with the creating 
     # and want to return to the main screen
+    done_button = ft.Container(
+        content = ft.FilledButton(
+            content= ft.Text(
+                "Done",
+                size = 12,
+                color= st.TEXT_COLOR_1,                
+                weight= ft.FontWeight.BOLD,    
+            ),
+            bgcolor= st.PLUS_BUTTON_COLOR,
+            style = ft.ButtonStyle(
+                shape= ft.RoundedRectangleBorder(radius=8),
+                padding=ft.Padding.symmetric(horizontal=12, vertical=5),
+            ),
+        ),
+        alignment= ft.Alignment.BOTTOM_CENTER
+    )
+    
 
 
-
-    page.add(name_input_display)
+    page.add(add_button)
+    page.add(done_button)
     page.window.resizable = False
     page.update()
 
